239a70a2e919b962044acd79e3dadd6d8a57f916
Subx1s0o <nazarsabardin@gmail.com>
2025-01-23 13:07:39 +0200

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.enableCors({
+        origin: ['http://localhost:3000'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
