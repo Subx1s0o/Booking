f6f8c31e655379801d39fe0f3b6ca024556c3aa9
Subx1s0o <nazarsabardin@gmail.com>
2025-01-24 16:04:27 +0200

    feat: update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin: ['http://localhost:3000'],
+        origin: [
+            'http://localhost:3000',
+            'https://booking-chi-umber.vercel.app',
+        ],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     })
     app.useGlobalPipes(
